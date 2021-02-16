f7338d6f3338af10dd3fd03d95df8be2cd54fc3f
Edvards Jānis Rečickis <edvards.recickis@scandiweb.com>
2021-02-16 11:47:45 +0200

    Prevent Google Analytics from getting triggered twice

--- view/frontend/web/js/google-analytics-mixin.js
+++ view/frontend/web/js/google-analytics-mixin.js
@@ -3,15 +3,16 @@ define(['mage/utils/wrapper'], function (wrapper) {
 
     return function (gaFunction) {
         return wrapper.wrap(gaFunction, function (parentMethod, config) {
-            window.addEventListener('CookiebotOnAccept', function () {
-                if (Cookiebot.consent.statistics) {
-                    parentMethod(config);
-                }
-            });
             if (typeof Cookiebot === 'undefined') {
                 return;
             }
-            if (Cookiebot.consent.statistics) {
+            if (!Cookiebot.hasResponse) {
+                window.addEventListener('CookiebotOnAccept', function () {
+                    if (Cookiebot.consent.statistics) {
+                        parentMethod(config);
+                    }
+                });
+            } else if (Cookiebot.consent.statistics) {
                 parentMethod(config);
             }
         })

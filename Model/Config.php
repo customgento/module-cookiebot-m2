6cb531bf3f4f342916cb3c9356ab4700ec93b197
Wilfried Wolf <wilfried.wolf@sandstein.de>
2021-03-04 18:09:56 +0100

    retrieves store specific config

--- Model/Config.php
+++ Model/Config.php
@@ -1,10 +1,10 @@
 <?php
-
 declare(strict_types=1);
 
 namespace CustomGento\Cookiebot\Model;
 
 use Magento\Framework\App\Config\ScopeConfigInterface;
+use Magento\Store\Model\ScopeInterface;
 
 class Config
 {
@@ -24,16 +24,16 @@ class Config
 
     public function isEnabled(): bool
     {
-        return $this->scopeConfig->isSetFlag(self::XML_PATH_COOKIEBOT_ENABLED);
+        return $this->scopeConfig->isSetFlag(self::XML_PATH_COOKIEBOT_ENABLED, ScopeInterface::SCOPE_STORE);
     }
 
     public function getId(): string
     {
-        return (string)$this->scopeConfig->getValue(self::XML_PATH_COOKIEBOT_ID);
+        return (string)$this->scopeConfig->getValue(self::XML_PATH_COOKIEBOT_ID, ScopeInterface::SCOPE_STORE);
     }
 
     public function getDataCulture(): string
     {
-        return (string)$this->scopeConfig->getValue(self::XML_PATH_DATA_CULTURE);
+        return (string)$this->scopeConfig->getValue(self::XML_PATH_DATA_CULTURE, ScopeInterface::SCOPE_STORE);
     }
 }

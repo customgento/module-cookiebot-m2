ea0940db102b435c309827c64510a6332eeab5d1
Iman Aboheydary <iman@customgento.com>
2025-04-29 16:21:49 +0200

    Add video block mixins, DEV-1111
    Signed-off-by: Iman Aboheydary <iman@customgento.com>

--- view/frontend/requirejs-config.js
+++ view/frontend/requirejs-config.js
@@ -3,6 +3,9 @@ var config = {
         mixins: {
             'Magento_GoogleAnalytics/js/google-analytics': {
                 'CustomGento_Cookiebot/js/google-analytics-mixin': true
+            },
+            'Magento_ProductVideo/js/load-player': {
+                'CustomGento_Cookiebot/js/load-player-mixin': true
             }
         }
     }

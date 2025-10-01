66a369f760816a6b8b34f7a5f7cc3756551300a4
Iman Aboheydary <iman@customgento.com>
2025-10-01 12:59:03 +0200

    Block sliders, DEV-1111

--- view/frontend/requirejs-config.js
+++ view/frontend/requirejs-config.js
@@ -6,6 +6,9 @@ var config = {
             },
             'Magento_ProductVideo/js/fotorama-add-video-events': {
                 'CustomGento_Cookiebot/js/fotorama-video-events-mixin': true
+            },
+            'Magento_PageBuilder/js/resource/jarallax/jarallax-video': {
+                'CustomGento_Cookiebot/js/jarallax-video-mixin': true
             }
         }
     }
--- view/frontend/web/js/fotorama-video-events-mixin.js
+++ view/frontend/web/js/fotorama-video-events-mixin.js
@@ -1,6 +1,7 @@
 define([
     'jquery',
-], function ($) {
+    'CustomGento_Cookiebot/js/video-blocker-widget'
+], function ($, createVideoBlocker) {
     'use strict';
 
     return function (widget) {
@@ -14,29 +15,9 @@ define([
                 }
 
                 const videoElement = event.target.querySelector('.product-video');
-                const divElement = document.createElement('div');
-                const paragraphElement = document.createElement('p');
-                const iframeHeight = videoElement?.getBoundingClientRect().height || 300;
-                const iframeWidth = videoElement?.getBoundingClientRect().width || 400;
-
-                divElement.innerHTML = `
-                                        <div style="background-color:#CCC;display:inline-block;height:${iframeHeight}px;position:relative;width:${iframeWidth}px; z-index: 1000;">
-                                            <div style="background-color:#848484;border-radius:15px;height:50%;position:absolute;transform:translate(50%,50%);width:50%;">
-                                                <p style="color:#FFF;font-size:7.5em;position:relative;top:50%;left:50%;margin:0;text-align:center;transform:translate(-50%,-50%);">&ctdot;</p>
-                                            </div>
-                                        </div>
-                                    `;
-
-                divElement.classList.add('cookieconsent-optout-marketing');
-                paragraphElement.innerHTML =
-                    $.mage.__('Please <a href="javascript:Cookiebot.renew()">accept marketing cookies</a> to view this content.')
-                ;
-
-                divElement.style.fontSize = '1.4rem';
-                divElement.append(paragraphElement);
-                paragraphElement.style.zIndex = '10001';
-                paragraphElement.style.position = 'relative';
-                videoElement?.parentNode.insertBefore(divElement, videoElement);
+                
+                // Use the video blocker widget
+                createVideoBlocker(videoElement);
             },
 
             _initialize: function () {

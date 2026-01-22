b852178d7a79c1e5b7567832a1cd2d5325aca05d
Iman Aboheydary <iman@customgento.com>
2026-01-22 11:20:10 +0100

    Fix csp errrors, DEV-1113
    Signed-off-by: Iman Aboheydary <iman@customgento.com>

--- Model/ScriptGenerator.php
+++ Model/ScriptGenerator.php
@@ -4,6 +4,8 @@ declare(strict_types=1);
 
 namespace CustomGento\Cookiebot\Model;
 
+use Magento\Csp\Helper\CspNonceProvider;
+
 class ScriptGenerator
 {
     private const COOKIEBOT_SCRIPT_FORMAT = '<script
@@ -12,6 +14,7 @@ class ScriptGenerator
             src="https://consent.cookiebot.com/uc.js"
             data-cbid="%s"
             %s
+            %s
             type="text/javascript" async></script>';
     private const EU_COOKIEBOT_SCRIPT_FORMAT = '<script
             id="Cookiebot"
@@ -19,16 +22,13 @@ class ScriptGenerator
             src="https://consent.cookiebot.eu/uc.js"
             data-cbid="%s"
             %s
+            %s
             type="text/javascript" async></script>';
 
-    /**
-     * @var Config
-     */
-    private $config;
-
-    public function __construct(Config $config)
-    {
-        $this->config = $config;
+    public function __construct(
+        private readonly Config $config,
+        private readonly CspNonceProvider $cspNonceProvider
+    ) {
     }
 
     public function generate(): string
@@ -36,11 +36,12 @@ class ScriptGenerator
         $cookiebotId = $this->config->getId();
         $dataCulture = $this->config->getDataCulture() ?
             sprintf('data-culture="%s"', $this->config->getDataCulture()) : '';
+        $nonce       = sprintf('nonce="%s"', $this->cspNonceProvider->generateNonce());
 
         if ($this->config->useEuCdn()) {
-            return sprintf(self::EU_COOKIEBOT_SCRIPT_FORMAT, $cookiebotId, $dataCulture);
+            return sprintf(self::EU_COOKIEBOT_SCRIPT_FORMAT, $cookiebotId, $dataCulture, $nonce);
         }
 
-        return sprintf(self::COOKIEBOT_SCRIPT_FORMAT, $cookiebotId, $dataCulture);
+        return sprintf(self::COOKIEBOT_SCRIPT_FORMAT, $cookiebotId, $dataCulture, $nonce);
     }
 }

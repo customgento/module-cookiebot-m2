e7951e1c0e256fa580b5569e9681f08b152b8893
Iman Aboheydary <iman@customgento.com>
2026-01-23 11:28:17 +0100

    Fix csp errrors, DEV-1113
    Signed-off-by: Iman Aboheydary <iman@customgento.com>

--- Model/ScriptGenerator.php
+++ Model/ScriptGenerator.php
@@ -14,14 +14,12 @@ class ScriptGenerator
             src="https://consent.cookiebot.com/uc.js"
             data-cbid="%s"
             %s
-            %s
             type="text/javascript" async></script>';
     private const EU_COOKIEBOT_SCRIPT_FORMAT = '<script
             id="Cookiebot"
             data-cfasync="false"
             src="https://consent.cookiebot.eu/uc.js"
             data-cbid="%s"
-            %s
             %s
             type="text/javascript" async></script>';
 
@@ -36,12 +34,11 @@ class ScriptGenerator
         $cookiebotId = $this->config->getId();
         $dataCulture = $this->config->getDataCulture() ?
             sprintf('data-culture="%s"', $this->config->getDataCulture()) : '';
-        $nonce       = sprintf('nonce="%s"', $this->cspNonceProvider->generateNonce());
 
         if ($this->config->useEuCdn()) {
-            return sprintf(self::EU_COOKIEBOT_SCRIPT_FORMAT, $cookiebotId, $dataCulture, $nonce);
+            return sprintf(self::EU_COOKIEBOT_SCRIPT_FORMAT, $cookiebotId, $dataCulture);
         }
 
-        return sprintf(self::COOKIEBOT_SCRIPT_FORMAT, $cookiebotId, $dataCulture, $nonce);
+        return sprintf(self::COOKIEBOT_SCRIPT_FORMAT, $cookiebotId, $dataCulture);
     }
 }

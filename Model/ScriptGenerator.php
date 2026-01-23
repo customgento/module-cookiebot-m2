b7851614511faf297a542733f2f8d6cab3781cc3
Iman Aboheydary <iman@customgento.com>
2026-01-23 16:37:04 +0100

    Remove extra changes, DEV-1113
    Signed-off-by: Iman Aboheydary <iman@customgento.com>

--- Model/ScriptGenerator.php
+++ Model/ScriptGenerator.php
@@ -4,8 +4,6 @@ declare(strict_types=1);
 
 namespace CustomGento\Cookiebot\Model;
 
-use Magento\Csp\Helper\CspNonceProvider;
-
 class ScriptGenerator
 {
     private const COOKIEBOT_SCRIPT_FORMAT = '<script
@@ -14,7 +12,6 @@ class ScriptGenerator
             src="https://consent.cookiebot.com/uc.js"
             data-cbid="%s"
             %s
-            %s
             type="text/javascript" async></script>';
     private const EU_COOKIEBOT_SCRIPT_FORMAT = '<script
             id="Cookiebot"
@@ -22,12 +19,10 @@ class ScriptGenerator
             src="https://consent.cookiebot.eu/uc.js"
             data-cbid="%s"
             %s
-            %s
             type="text/javascript" async></script>';
 
     public function __construct(
-        private readonly Config $config,
-        private readonly CspNonceProvider $cspNonceProvider
+        private readonly Config $config
     ) {
     }
 
@@ -36,12 +31,11 @@ class ScriptGenerator
         $cookiebotId = $this->config->getId();
         $dataCulture = $this->config->getDataCulture() ?
             sprintf('data-culture="%s"', $this->config->getDataCulture()) : '';
-        $nonce       = sprintf('nonce="%s"', $this->cspNonceProvider->generateNonce());
 
         if ($this->config->useEuCdn()) {
-            return sprintf(self::EU_COOKIEBOT_SCRIPT_FORMAT, $cookiebotId, $dataCulture, $nonce);
+            return sprintf(self::EU_COOKIEBOT_SCRIPT_FORMAT, $cookiebotId, $dataCulture);
         }
 
-        return sprintf(self::COOKIEBOT_SCRIPT_FORMAT, $cookiebotId, $dataCulture, $nonce);
+        return sprintf(self::COOKIEBOT_SCRIPT_FORMAT, $cookiebotId, $dataCulture);
     }
 }
